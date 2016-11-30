a769af6d2304330aef18ec2ed83d57c98dd83d59
Rikarox <djrikarox@gmail.com>
2016-11-30 09:15:19 +0100

    Add Project

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+	// ints,string,float,chars here..
+	
+	
+	public static void intro(){
+		//animejšn
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		// System.out.printf("Loading shop.");
+		// TimeUnit.SECONDS.sleep(1);    sleepo
+		
+		//intro Shop text, wait 5 sec   --------------------------------------------------------------------------
+			
+		intro(); 
+		
+		
+		//Main menu ----------------------------------------------------------------------------------------------
+	   //List, Kupit tovar, Predaj tovar (*Optional -> Random product sell), 
+		//zostatok na ucte bude ukazovat v menu
+		
+		/* Zostatok : 5000 €
+		 * 1) Products
+		 * 2) Buy product
+		 * 3) Sell product
+		 * 4) Sell Current product
+		 * 5) Settings
+		 * 6) Credits
+		 * 7) Exit
+		 */
+		
+
+		
+		
+		
+		
+	}
+
+}
