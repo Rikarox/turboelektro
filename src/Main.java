9233f25888743ff9a6ae637700dd41d0c3933c53
Rikarox <djrikarox@gmail.com>
2016-12-04 19:34:50 +0100

    Setting up Skeleton

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,69 @@
+/*
+ *  TURBOSHOP ULTRA BEAST ELECTRO SHOP @miko @bundy @roxor
+ */
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-	// ints,string,float,chars here..
+	// ints,string,float,chars here.. dame ich ako public do mainu
 	
-	
-	public static void intro(){
-		//animejšn
+	// srandicky na intro loading a pod. casove hluposti
+	public static void intro() throws InterruptedException{
+		System.out.printf("Loading shop.");
+		TimeUnit.SECONDS.sleep(1);   
 	}
 	
+	
+	/* ______________________________________________________
+	 * menuShow() 
+	 * 1) Products
+	 * 2) Buy product
+	 * 3) Random Sell //Simulacia kupujuceho
+	 * 4) Sell product
+	 * 5) Settings
+	 * 6) Credits
+	 * 7) Exit
+	 * ______________________________________________________
+	 */
+	
+	
+	/*______________________________________________________ 
+	 * line()
+	 * vypise medzeru
+	 * vypise odelovaciu ciaru
+	 * HASH to int money !!!! o to sa postara rikarox.. 
+	 * companyName zo suboru compname, money zo suboru compmoney
+	 * dalsia odelovacia ciara
+	 *______________________________________________________
+	 */
+	
+	
+	/*______________________________________________________ 
+	 * nameSet()
+	 * scanner na string nameSet
+	 * nameSet = companyName
+	 * vytvorenie suboru compname
+	 * zapisanie mena do suboru 
+	 * vytvorenie suboru compmoney
+	 * 5000 convert to HASH  !!!! O to sa postara rikarox..
+	 * zapisanie hashu do compmoney
+	 * nameOn = 1
+	 *______________________________________________________
+	 */
+	
+	
 	public static void main(String[] args) throws InterruptedException {
-		// System.out.printf("Loading shop.");
-		// TimeUnit.SECONDS.sleep(1);    sleepo
-		
-		//intro Shop text, wait 5 sec   --------------------------------------------------------------------------
+		//if boolean nameOn = 1 pokracuj, else nameSet();
 			
-		intro(); 
+		//intro(); 
+		//line();
+		
+		//menuShow(); <- vypis moznosti
+		// Scanner na int vyberMenu (1,2,3,4,5,6,7, if other do printf prazdnu medzeru)
+		
+		// line();
 		
 		
-		//Main menu ----------------------------------------------------------------------------------------------
-	   //List, Kupit tovar, Predaj tovar (*Optional -> Random product sell), 
-		//zostatok na ucte bude ukazovat v menu
-		
-		/* Zostatok : 5000 €
-		 * 1) Products
-		 * 2) Buy product
-		 * 3) Sell product
-		 * 4) Sell Current product
-		 * 5) Settings
-		 * 6) Credits
-		 * 7) Exit
-		 */
 		
 
 		
